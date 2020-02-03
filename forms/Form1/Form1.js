21a169d921f6697d68da6d857f1a77592786401d
valeriegamon <valeriegamon@valeries-mbp.ddns.creighton.edu>
2020-02-03 16:58:30 -0600

    ssss

--- forms/Form1/Form1.js
+++ forms/Form1/Form1.js
@@ -5,5 +5,5 @@ btnShowMessage.onclick=function(){
 console.log(`My best friend is ${names[0]}`); 
 lblMessage.textContent = `Another friend is ${names[2]}`
 alert(`The second friend is named ${names[1]}`);
-alert(`The second friend is named YAY!`);
+alert(`The second friend is named YAYS!`);
 }
